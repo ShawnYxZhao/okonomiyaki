64eb5ba801152d67c8e7774fb519630a5a90d5b2
Shawn Zhao <97186212+ShawnYxZhao@users.noreply.github.com>
2022-03-18 21:53:58 -0400

    Add infor buttons to home

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
 
 const Button = (props) => {
-    const { icon="", text="", route="/" } = props;
-    return (
-        <Link to={route}>
-            <div className="cursor-pointer flex gap-1 hover:bg-neutral-100 px-5 py-1 rounded-lg w-fit">
-                    {icon}
-                    {text}
-            </div>
-        </Link>
-    );
+    const { icon="", text="", route="/", type="NAV" } = props;
+    switch(type) {
+        case "NAV":
+            return (
+                <Link to={route}>
+                    <div className="cursor-pointer flex gap-1 hover:bg-neutral-100 px-5 py-1 rounded-lg w-fit">
+                        <div className=" self-center">{icon}</div>
+                        {text}
+                    </div>
+                </Link>
+            );
+        case "BIO":
+            return (
+                <Link to={route}>
+                    <div className="border border-gray-300 bg-white cursor-pointer 
+                    hover:shadow-md rounded-md flex gap-1 justify-center py-1">
+                        <div className="self-center">{icon}</div>
+                        {text}
+                    </div>
+                </Link>
+            );
+    }
 }
 
 export default Button;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,10 @@ import TypeAnimation from 'react-type-animation';
 // Components
 import Button from '../components/Button';
 
+// Icons
+import { FaGithub, FaLinkedin, FaExpand } from 'react-icons/fa'; 
+import { SiRiotgames } from 'react-icons/si'
+
 const Home = () => {
     return (
         <div className="">
@@ -16,7 +20,7 @@ const Home = () => {
                 <TypeAnimation
                     cursor={false}
                     repeat={3}
-                    sequence={['blogger;', 3000, 'traveller;', 3000, 'developer;', 3000]}
+                    sequence={['gamer;', 3000, 'traveller;', 3000, 'developer;', 3000]}
                     className="underline decoration-orange-300"
                     wrapper="span"
                 />
@@ -24,8 +28,19 @@ const Home = () => {
                     Previously at <span className='text-red-500 font-normal'>Nextroll</span> as Frontend Engineer, <span className='text-lime-500 font-normal'>Oanda</span> as Fullstack Developer;</div>
                 </div>
             </div>
-            <div>
-                <Button text="Github"></Button>
+            <div className='grid grid-cols-5 gap-4 mt-3'>
+                <div className='text-content'>
+                    <Button type="BIO" icon={<FaGithub className=''/>} text="Github"></Button>
+                </div>
+                <div className='text-content'>
+                    <Button type="BIO" icon={<FaLinkedin className='text-blue-500'/>} text="LinkedIn"></Button>
+                </div>
+                <div className='text-content'>
+                    <Button type="BIO" icon={<SiRiotgames className='text-red-500'/>} text="League"></Button>
+                </div>
+                <div className='text-content col-start-4 col-span-2'>
+                    <Button type="BIO" icon={<FaExpand className=''/>} text="More"></Button>
+                </div>
             </div>
         </div>
     );
